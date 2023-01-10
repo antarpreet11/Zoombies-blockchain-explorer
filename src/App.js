08a1930aaa62b62b1bc25ba96b8149e9b6ca1ca5
antarpreet11 <singhantarpreet11@gmail.com>
2023-01-10 08:29:40 -0800

    Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import { useEthers } from '@usedapp/core';
 import DisplayInfo from './components/DisplayInfo';
 
 function App() {
-  const { activateBrowserWallet, account , chainId} = useEthers();
+  const { activateBrowserWallet, account , chainId, deactivate} = useEthers();
   const isConnected = account !== undefined;
   console.log("App called");
   return (
-      <div>
+      <div class="d-flex justify-content-center mt-5">
         {isConnected ? 
-          <div>
-            <button>Connected!</button>
+          <div class="justify-content-center">
+            <button onClick={deactivate} type="button" class="btn btn-success btn-lg mb-2">Connected!</button>
             {account && <p>Account: {account}</p>}
             <DisplayInfo acc={account} chID={chainId}></DisplayInfo>
           </div> :
-          <div>
-            <button onClick={() => activateBrowserWallet()}>Connect</button>
+          <div class="justify-content-center">
+            <button onClick={() => activateBrowserWallet()} type="button" class="btn btn-warning btn-lg">Connect</button>
           </div>
         }
       </div>
 
-  );
+  ); 
 }
 
 export default App;
--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEtherBalance , useBlockMeta , useToken} from '@usedapp/core';
+import { formatEther } from '@ethersproject/units'
 
 const DisplayInfo = (props) => {
     const balance = useEtherBalance(props.acc);
@@ -7,8 +8,8 @@ const DisplayInfo = (props) => {
     // const tokeninfo = useToken(props.acc);
     return (
         <div>
-            <div>Chain ID: {props.chID !== undefined ? props.chID.toString() : "undefined"}</div>
-            <div>Currect Balance: {balance !== undefined ? balance.toString() : "undefined"} wei</div>
+            <div>Chain ID: {props.chID !== undefined ? props.chID : "undefined"}</div>
+            <div>Currect Balance: {balance !== undefined ? formatEther(balance) : "undefined"}</div>
             <div>Current Block Number: {info.blockNumber !== undefined ? info.blockNumber.toString() : "undefined"}</div>
             <div>Last Time Stamp: {info.timestamp !== undefined ? info.timestamp.toString() : "undefined"}</div>
         </div>
