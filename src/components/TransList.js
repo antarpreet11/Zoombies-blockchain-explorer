d4a7fd2930b2a71bf296f7f7f5b3593abcf3e98a
antarpreet11 <singhantarpreet11@gmail.com>
2023-01-12 14:45:51 -0800

    Can display both transfer lists now accurately

--- src/components/TransList.js
+++ src/components/TransList.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { ethers } from 'ethers'
 import { formatEther } from '@ethersproject/units'
 import { useState } from 'react'
-import DispTransList from './DispTransList.js'
 
 const TransList = (props) => {
 
     const [ztransactions, setZtransactions] = useState([]);
     const [zbtransactions, setZbtransactions] = useState([]);
 
-    props.contractzoom.on("Transfer", async function(from, to, amount, event) {
+    props.contractzoom.on("Transfer", (from, to, amount, event) => {
         const t1 = {
             from: from, 
             to: to, 
@@ -22,16 +21,15 @@ const TransList = (props) => {
             const newarr = [t1, ...prev];
             return newarr.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i);
         })
-        console.log(ztransactions);
         props.zsupply();
         props.zbsupply();
     });
 
-    props.contractzoombies.once("Transfer", (from, to, amount, event) => {
+    props.contractzoombies.on("Transfer", (from, to, tokenID, event) => {
         const t1 = {
             from: from, 
             to: to, 
-            amount: formatEther(amount),
+            tokenID: tokenID.toString(),
             id: event.transactionHash, 
             key: event.transactionHash,
             data: event
@@ -45,10 +43,25 @@ const TransList = (props) => {
     });
         
     return (
-        <div>
-            <ul>
-                {ztransactions.map((item) => <li key={item.key}>{item.id}</li>)}
-            </ul>
+        <div class="d-flex flex-row justify-content-between mt-3">
+            <div>
+                <div>Zoom Transfers List: </div>
+                <ul class="list-group">
+                {
+                    ztransactions.map((item) => 
+                    <li key={item.key} class="list-group-item border border-primary">{item.id}</li>)
+                }
+                </ul>
+            </div>
+            <div>
+                <div>Zoombies Transfers List: </div>
+                <ul class="list-group">
+                {
+                    zbtransactions.map((item) => 
+                    <li key={item.key} class="list-group-item border border-primary">{item.tokenID}</li>)
+                }
+                </ul>
+            </div>
         </div>
     )
 }
